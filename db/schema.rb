ac81e23f0232dc52d981d1082c4c5f78e1042f7d
nickFalcone <nfalcone@u.rochester.edu>
2014-10-16 19:51:43 -0400

    added adapter for postgresql in database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141004162558) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: true do |t|
     t.string   "fName"
     t.string   "lName"
@@ -24,6 +27,6 @@ ActiveRecord::Schema.define(version: 20141004162558) do
     t.string   "password_digest"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
