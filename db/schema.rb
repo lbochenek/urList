741113bc4924e370862108da13b52643c5056211
nickFalcone <nfalcone@u.rochester.edu>
2014-11-01 14:01:24 -0400

    fixed lists

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141031160002) do
+ActiveRecord::Schema.define(version: 20141101154429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 20141031160002) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "types", ["category"], name: "index_types_on_category", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "fName"
