a1f693cd19c8dce0f46092546892fcc13ac076ab
Laura Bochenek <laura.bochenek@gmail.com>
2014-11-20 10:18:29 -0500

    ajax picture uploads yay

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,6 +3,7 @@ class Listing < ActiveRecord::Base
   belongs_to :user
   belongs_to :type
   default_scope -> { order('created_at DESC') }
+  mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :title, presence: true, length: { maximum: 50 }
   validates :content, presence: true, length: { minimum: 15, maximum: 1000 }
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141101154429) do
+ActiveRecord::Schema.define(version: 20141120002616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20141101154429) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "type_id"
+    t.string   "picture"
   end
 
   add_index "listings", ["type_id"], name: "index_listings_on_type_id", using: :btree
@@ -35,8 +36,6 @@ ActiveRecord::Schema.define(version: 20141101154429) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  add_index "types", ["category"], name: "index_types_on_category", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "fName"
