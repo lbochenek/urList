d30d878f4a3f3ae7cdd19406255db96dab9a01b7
Laura Bochenek <laura.bochenek@gmail.com>
2014-11-20 15:56:01 -0500

    stricter image uploading
    images are now resized when uploaded
    limits on image type, size

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,6 +1,9 @@
 # encoding: utf-8
 
 class PictureUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [400, 400]
+  
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
@@ -14,6 +17,10 @@ class PictureUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+  
+  def extension_white_list
+    %w(jpg jpeg gif png)
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
