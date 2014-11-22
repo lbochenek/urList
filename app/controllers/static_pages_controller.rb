512d49ef0742b7555ef9b4bf104f1a025c7b0c23
Laura Bochenek <laura.bochenek@gmail.com>
2014-11-22 16:32:54 -0500

    style changes / clean up

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,18 +3,7 @@ class StaticPagesController < ApplicationController
     if logged_in?
       @listing = current_user.listings.build
       @feed_items = Listing.all
-      # @feed_items = Listing.all.paginate(page: params[:page])
     end
     
   end
-
-  def help
-  end
-  
-  def about
-  end
-  
-  def contact
-  end
-    
 end
