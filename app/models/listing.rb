2fc9e86a9f2c08035c9ff69ceb026a3b280fd157
Laura Bochenek <laura.bochenek@gmail.com>
2014-11-22 13:41:40 -0500

    pictures can be deleted, cheap fix for picture error messages

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -9,7 +9,7 @@ class Listing < ActiveRecord::Base
   validates :content, presence: true, length: { minimum: 15, maximum: 1000 }
   validates :type_id, presence: true
   validate :picture_size
-      
+        
   def picture_size
     if picture.size > 1.megabytes
       errors.add(:picture, "should be less than 1MB")
